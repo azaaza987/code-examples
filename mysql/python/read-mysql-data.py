01972db9a5567c3582c42bef5cd50cc68d99b8d6
Shawn <azaaza987@gmail.com>
2018-11-18 18:14:19 +0800

    Initial commit

---
+++ mysql/python/read-mysql-data.py
@@ -0,0 +1,22 @@
+import datetime
+import random
+import MySQLdb
+import time
+
+db = MySQLdb.connect('localhost', 'root', 'root', 'testdb')
+
+con = db.cursor()
+con.execute("SELECT * from myusers")
+
+rows = con.fetchall()
+
+for row in rows:
+    a_name = row[0]
+    a_surnames = row[1]
+    a_country = row[2]
+    a_gender = row[3]
+    a_os = row[4]
+    a_car = row[5]
+
+    print (a_name + '-----' + a_surnames  + '-----' + a_country  + '-----' + a_gender  + '-----' + a_os  + '-----' +  a_car)
+
